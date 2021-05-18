ed5800d90c3783f6d3aaff6ba5e2720a7e386053
Dmitry Ihnatiev <straenger@gmail.com>
2021-05-18 19:41:16 +0300

    edited

--- Contents.swift
+++ Contents.swift
@@ -29,18 +29,18 @@ print (Double.max)
 print (Double.min)
 
 //MARK: - 2. Записать число 16 в 2 системе исчисления 0bxxxxx
-let value16 = 16
+let value16 = 0b1000
 let str = String(value16, radix: 2)
-//print("0b" + str )
+print("0b" + str )
 // спросить про это задание
 //MARK: - Сделать 2 переменные с  bool значениями, и вывести все возможные их сочетания через if c использованием ( || and &&).  В чем разница между & and &&?
 
 var pswd = true
-var key = false
+var key = true
 //для более сложного оператора создал 3 переменную
 var pin = true
 
-if pswd && key {
+if !(pswd) && key {
 print("Welcome!")
 } else {
 print("accessDenied")
@@ -99,18 +99,40 @@ print( "Value of fourth element is \(tenTimeFive[3])" )
 print( "Value of six element is \(tenTimeFive[5])" )
 
 //MARK: - 9. Создать массив который нельзя изменять/можно изменять
+
 //MARK: - 10. Создать массив из 10 повторяющихся двоек, вывести элемент и его индекс
 let tenTimeTwo = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 for (index, value) in tenTimeTwo.enumerated() {
    print("\(index) - \(value)")
 }
 //MARK: - 11. Заполнить массив случайными числами от 1 до 20, найти на каком месте находится 4 вывести это место и закончить поиск, или вывести “4 не найдена” в противном случае
-let randomNumber = [Int](1...20)
 
+var randomNumber: [Int] = []
+for  _ in 0..<20{
+    let rndNumber = Int.random(in: 1...20)
+    randomNumber.append(rndNumber)
+}
+print(randomNumber)
+let four = 0
+
+//randomNumber.index(of: 4)   --------
+for number in randomNumber.enumerated(){
+    number.offset
+    number.element
+    //    print(number.offset) // порядковый номер в массиве
+    //    print(number.element) // значение номер в массиве
+    
+    if number.element == 4 {
+        print(number.offset)
+        
+    }else if number.offset == 19{
+        print("4 не найдена")
+    }
+}
 //MARK: - 12. Создать множество с элементами [1,3,7,8] и [12,3,76,8] найти элементы которые присутствую в обоих множествах, присутствую в первом, но отсутствуют во втором.
 let someSet1: Set = [1,3,7,8]
 let someSet2: Set = [12,3,76,8]
-someSet1.intersection(someSet2).sorted()
+someSet1.intersection(someSet2)
 
 //MARK: - 13. Создать словарь [“21”: “some element”, “232”: “some element”, “34”: “some element”] вывести все ключи и элементы попарно
 var dictionary = ["21": "some element", "232": "some element", "34": "some element"]
@@ -119,10 +141,15 @@ for (number, value) in dictionary {
     print("\(number) - \(value)")
 }
 //MARK: - Сделать функцию, которая принимает в себя строку “abc” и изменяет ее на “abcd”, функция не должна возвращать значение
-func changeValue1 (string: String){
-        print("\(string)d")
-    }
-changeValue1(string: "abc")
+
+func addD (letter: inout String){
+    letter += "d"
+}
+var abc = "abc"
+addD(letter: &abc)
+
+//inout
+
 
 //MARK: - Сделать функцию с не именованным  argument label которая считает сумму двух чисел
 
@@ -147,33 +174,48 @@ func someStrings(manyString: String...){
     for x in manyString {
         print(" the string is \(x)")
     }
+    var result2 = " "
+    for y in manyString{
+        result2 += y
+    }
+    print(result2)
 }
 someStrings(manyString: "what", "the", "duck")
 
 
-//func greeting(for person: String) -> String {
-//    "Hello, "  + person + "!"
-//}
-//print(greeting(for: "Dave")
-
 //MARK: - Сделать функцию складывает два числа и принимает дополнительную функцию, которая принимает Int и умножает на 2 аргумент, применить функцию к двум аргументам.
 
-//func operate1 (num1: Int, num2: Int) -> Int{
-//    func operate2 (num3: Int) -> Int{
-//    let operate3 = (num3 * 2)
-//        return operate1(num1: <#T##Int#>, num2: <#T##Int#>)
-//
+func operate1 (num1: Int, num2: Int, num3: (Int) -> Int ) -> Int{
+    num3(num1)
+    num3(num2)
+var num4 = num3(num1) + num3(num2)
     
+ return num4
+}
+operate1(num1: 2, num2: 4, num3: {$0 * 2})
 
 
 //MARK: -  Создать функцию которая считает сумму элементов массива который передан,  создать функцию внутри функции, которая принимает на вход число и если оно больше 3 печатает его.
-//
-//func numbers (smArr: [Int]) -> Int {
-//    func numbers2
-//    if numbers > 3{
-//        print (numbers)
-//    }
-//}
+
+func someArray(array: [Int]){
+    var count = 0          // порядковый номер
+    var arrayCount = 0    // наш счет
+    while count != array.count {
+    arrayCount = arrayCount + array[count]
+        count += 1
+    }
+print(arrayCount)
+}
+someArray(array: [1, 2, 3, 4, 5])
+
+
+
+
+var i = 0
+while i < 6 {
+    i += 1
+print(i)
+}
 
 
 
